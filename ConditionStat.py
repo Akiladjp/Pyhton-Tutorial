42e8e36b9bbc647f96c82dcb34a559e24727abcb
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-07-09 02:41:26 +0530

    1st commit

---
+++ ConditionStat.py
@@ -0,0 +1,12 @@
+marks = 70;
+
+if(marks >= 75):
+    print('A Pass')
+elif(marks >= 65):
+    print('B Pass')
+elif(marks >= 55):
+    print('C pass')
+else:
+    print('fall')
+
+
---
+++ Function.py
@@ -0,0 +1,4 @@
+name, age, score = 'Kamal', 25, 56.76
+
+print("He is %s, Age is %d" %(name, age))
+print(f"He is {name}, Age is {age}")
