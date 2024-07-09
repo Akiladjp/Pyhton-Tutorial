e6cb8ea94b7b8da3a5960266433eac827389e8d1
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-07-10 03:05:43 +0530

    2nd commit

--- Function.py
+++ Function.py
@@ -1,4 +1,43 @@
-name, age, score = 'Kamal', 25, 56.76
+name = "CodePRO tutorial"
 
-print("He is %s, Age is %d" %(name, age))
-print(f"He is {name}, Age is {age}")
+print(name.upper())
+print(name.lower())
+
+print(name.title())
+print(name.capitalize())
+
+# find() and index()
+
+print(name.find('Co'))
+print(name.index('Co'))
+
+print(name.center(20))
+print(name.ljust(20),"-")
+print(name.rjust(20), "-")
+
+
+my = '----MSI----'
+
+print(my)
+print(my.strip())
+print(my.lstrip('-'))
+print(my.strip('-'))
+
+
+name = "codepro lk"
+
+print(name.startswith("code"))
+print(name.endswith('lk'))
+
+x = "kamal, 22, colombo"
+
+print(x.replace('22', '25'))
+
+x = 'abc'
+y = 'xyz'
+
+print(x.join(y))
+
+name = "nimal kamal saman"
+
+print(name.split())
---
+++ UserInput.py
@@ -0,0 +1,24 @@
+num1 = int(input('First number'))
+num2 = int(input('Second number'))
+
+answer = num1 + num2
+
+
+print(answer)
+
+Reply = "CodePRO"
+print(Reply[4:7])
+
+s1 = "python"
+s2 = 43
+
+print(s1 + " " + str(s2))
+
+
+print("py" in s1)
+
+
+name, age, score = 'Kamal', 25, 56.76
+
+print("He is %s, Age is %d" %(name, age))
+print(f"He is {name}, Age is {age}")
