51237a0d584a50a3b15bc2ba4da915b46fa37cde
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-07-16 02:25:22 +0530

    list

---
+++ List.py
@@ -0,0 +1,31 @@
+my_list = ['kamal', 20, 'kandy', False, 20, [4, 5, 6]]
+
+print(my_list)
+print(my_list[3])
+print(my_list[1:3])     #print index 1 element and index 2 element
+
+print(my_list[5][2])
+
+my_list[2] = 'colombo'
+
+print(my_list)
+
+my_list.append("Saman")
+
+print(my_list)
+
+my_list.extend("saman")
+
+print(my_list)
+
+my_list.insert(0, 'hello')
+
+print(my_list)
+
+my_list.pop(1)
+
+my_list.remove(20)
+
+print(len(my_list))
+
+print(my_list.count(20))
