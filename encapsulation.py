b726694bd30e5365f37c4109748b384ed366772d
Akila Piyumal <92683626+Akiladjp@users.noreply.github.com>
2024-07-23 13:22:27 +0530

    encapsulation

---
+++ encapsulation.py
@@ -0,0 +1,21 @@
+# class myClass:
+#     x = 18
+#     __y = 20;
+#     def disp(self):
+#         return self.__y + self.x
+
+# myObj = myClass();
+# print(myObj.disp());
+
+
+class myClass:
+    def meth1(self):
+        print('hello')
+        self.__meth2()
+
+    def __meth2(self):
+        print('welcome')
+
+
+myObj = myClass();
+print(myObj.meth1());
---
+++ oop1.py
@@ -0,0 +1,6 @@
+class Phone:
+    def say(self, name):
+        print('Hello', name);
+
+phone1 = Phone();
+phone1.say('boss');
